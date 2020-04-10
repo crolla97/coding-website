a88efc82daf011c13a1b5c5aec4128862ae343ff
crolla97 <cristianocrolla@gmail.com>
2020-04-10 12:13:17 +0100

    Created header component with devise helper method

--- app/components/core/header/header_component.rb
+++ app/components/core/header/header_component.rb
@@ -1,8 +1,10 @@
 module Core
   module Header
     class HeaderComponent < ApplicationComponent
-      def initialize(*)
-        
+      
+      def initialize(**options)
+        @signed_in = options[:signed_in]
+        @signed_out = options[:signed_out]
       end
     end
   end
